ffe7e9076612e838fa1550614a823fe0ac97e3b9
mokka86coffee <43421744+mokka86coffee@users.noreply.github.com>
2019-01-20 13:58:48 +0300

    Add files via upload

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -3,13 +3,20 @@ const apiMiddleWare = store => next => action => {
                 fetch(`https://novaweb.studio/dashboard/_api/projects/${action.payload.id}`,
                         {
                                 method: "PATCH",
+                                mode: "cors",
                                 headers: { "Content-Type": "application/json" },
                                 body: JSON.stringify({ comment: action.payload.comment })
                         })
                         .then(res => next(action))
                         .catch(err => console.log('PATCH returned err: ', err));
         } else if (action.type === 'GET_PROJECTS') {
-                fetch('https://novaweb.studio/dashboard/_api/projects/?format=json')
+                fetch('https://novaweb.studio/dashboard/_api/projects/?format=json', {
+                        method: 'GET',
+                        mode: 'cors',
+                        headers: {
+                                "Content-Type": "application/json"
+                        }
+                })
                         .then(r => r.json())
                         .then(r => store.dispatch({ type: 'SORT_ARRAY', payload: { serverlist: r } }))
                         .catch(e => console.log('API NovaWeb JSON responded with ', e));
