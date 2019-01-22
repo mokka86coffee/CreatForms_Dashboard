c8f710286d2b84edfae4e3eeeafda3d4419bc1f8
mokka86coffee <43421744+mokka86coffee@users.noreply.github.com>
2019-01-22 15:11:36 +0300

    Add files via upload

--- src/reducers/apiNovawebReducer.js
+++ src/reducers/apiNovawebReducer.js
@@ -1,13 +1,17 @@
 import { getCiphers } from "crypto";
 
-const apiNovawebReducer = function (state = { template: '', serverlist: [], indexDrag: false, indexDragTo: false},action){
+const apiNovawebReducer = function (state = {},action){
     switch (action.type) {
         case 'EDIT_COMMENT': return { ...state, template: action.payload.el };
         case 'SUBMIT_COMMENT': return {...state, template: ''};
-        case 'SORT_ARRAY': return { ...state, serverlist: action.payload.serverlist };
+        case 'SORT_ARRAY': return { ...state, serverlist: action.payload.serverlist, error: null };
         case 'DRAG_START': return { ...state, indexDrag: action.payload.index };
         case 'DRAG_ENTER': return { ...state, indexDragTo: action.payload.index };
-        case 'DRAG_END': return { ...state, serverlist: action.payload.serverlist };
+        case 'DRAG_END': return { ...state, serverlist: action.payload.serverlist, indexDrag: -1, indexDragTo: -1  };
+        case 'TOUCH_START': return { ...state, indexDrag: action.payload.index, indexDragTo: action.payload.index, coords: {x: action.payload.coords.x, y: action.payload.coords.y}  };
+        case 'TOUCH_MOVE': return { ...state, coords: {x: action.payload.coords.x, y: action.payload.coords.y} };
+        case 'TOUCH_END': return { ...state, indexDrag: -1, indexDragTo: -1  };
+        case 'FETCH_ERROR': return { ...state, error: action.payload.error };
         default: return state;
     }
 }
