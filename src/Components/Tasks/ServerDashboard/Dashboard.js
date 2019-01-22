3825e5eb6918f19cf33ee96edf14bad19d4557a2
mokka86coffee <43421744+mokka86coffee@users.noreply.github.com>
2019-01-22 15:16:12 +0300

    Add files via upload

--- src/Components/Tasks/ServerDashboard/Dashboard.js
+++ src/Components/Tasks/ServerDashboard/Dashboard.js
@@ -6,6 +6,7 @@ class ServerDashboard extends React.Component {
     constructor(props) {
         super(props);
         this.timer = '';
+        this.touchChckTime={};
     }
 
     componentDidMount() {
@@ -17,29 +18,83 @@ class ServerDashboard extends React.Component {
         clearInterval(this.timer);
     }
 
+    componentDidCatch(){
+        console.log(arguments);
+    }
+
     editComment = (el) => this.props.dispatch({ type: 'EDIT_COMMENT', payload: { el } });
-    dragStart = (index) => this.props.dispatch({ type: 'DRAG_START', payload: { index, serverlist: this.props.projects.serverlist } });
+    dragStart = (index) => this.props.dispatch({ type: 'DRAG_START', payload: { index } });
     dragEnter = (index) => this.props.dispatch({type: 'DRAG_ENTER', payload: {index}});
     dragEnd = () => this.props.dispatch({ type: 'DRAG_END'});
 
+    touchStart = (e,index) => {
+            const w = parseInt(getComputedStyle(e.target).width.replace('px','')) / 2;
+            const h = parseInt(getComputedStyle(e.target).height.replace('px','')) / 2;
+            const x = e.touches[0].pageX;
+            const y = e.touches[0].pageY;
+            
+            this.touchChckTime = setTimeout(()=>{
+                this.props.dispatch({ type: 'TOUCH_START', payload: { index, coords: {x: x-w, y: y-h } } });
+        }, 300);
+    }
+    touchMove = (e) => {
+        const { projects: { coords: { x, y} } }  = this.props;
+        if(x&&y) {
+            this.props.dispatch({ type: 'TOUCH_MOVE', payload: { coords: {x: e.touches[0].pageX-50, y: e.touches[0].pageY-50 } } });
+        }
+    }
+    touchEnd = (e) => {
+        clearTimeout(this.touchChckTime);
+        const { projects: { coords: { x, y} } }  = this.props;
+        if(x&&y) {
+            let indexDragTo = document.elementsFromPoint(x,y).filter(el=>{
+                console.log(el.drag);
+                return el.dataset.drag;
+            });
+            document.elementsFromPoint(x,y).filter(el=>{
+                console.log(el.drag);
+                return el.dataset.drag;
+            });
+            console.log(x);
+            console.log(y);
+            console.log(indexDragTo);
+            if (indexDragTo) {
+                indexDragTo = indexDragTo[0].dataset.drag;
+                this.props.dispatch({ type: 'TOUCH_END', payload: { indexDragTo } });
+            }
+        }
+    }
+
     render() {
-        const { panelVisible, projects: { serverlist }} = this.props;
+        const { panelVisible, projects: { serverlist, error, indexDrag: iD, indexDragTo: iDt, coords: {x,y} } } = this.props;
+        console.log(this.props);
         return (
             <div className={panelVisible ? "dashboard" : "dashboard dashboard--hidden"}>
                 <div className="dashboard__projects">
                     <h4 className="dashboard__projects__title">{serverlist.length ? 'Проекты' : 'Загрузка...'}</h4>
+                    {error ? error.map(err=><p className="dashboard__projects__error" key={err.message.slice(0,4)}><em>Ошибка - {err.message}</em><br />Просто нужно подождать</p>) : null}
                     <div className="dashboard__projects__items">
-                        <div className="wrap_hidescroll">
+                        <div className="wrap_hidescroll" onScroll={e=>e.preventDefault()}>
                             {
                                 serverlist && serverlist.map((el,i) => (
                                     <div
                                         draggable="true" 
                                         key={el.id} 
-                                        className="item"
+                                        className={i==iD ? "item item--dragged" : i==iDt ? "item item--dragto" : "item"}
                                         onClick={() => this.editComment(el)}
                                         onDragStart={() => this.dragStart(i)}
                                         onDragEnter={() => this.dragEnter(i)}
                                         onDragEnd={() => this.dragEnd(i)}
+                                        onTouchStart={e=>this.touchStart(e,i)}
+                                        onTouchMove={e=>this.touchMove(e)}
+                                        onTouchEnd={e=>this.touchEnd(e)}
+                                        data-drag={i}
+                                        style={iD==i ? {
+                                            position: 'fixed',
+                                            top: y,
+                                            left: x,
+                                            width: '36vw'
+                                        }: {}}
                                     >
                                         <h5 className="item__title">{el.title}</h5>
                                         <p className="item__type"><em>Тип</em>{el.type}</p>
